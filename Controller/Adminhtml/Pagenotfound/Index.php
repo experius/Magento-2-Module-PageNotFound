a3153812b81f24b7d6f2faa0a8a11085a59b4e67
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-06-22 16:29:54 +0200

    [BUGFIX] admin form save issue, renamed to correct title

--- Controller/Adminhtml/Pagenotfound/Index.php
+++ Controller/Adminhtml/Pagenotfound/Index.php
@@ -30,7 +30,7 @@ class Index extends \Magento\Backend\App\Action
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
-            $resultPage->getConfig()->getTitle()->prepend(__("404 Pages"));
+            $resultPage->getConfig()->getTitle()->prepend(__("404 Reports"));
             return $resultPage;
     }
 }
