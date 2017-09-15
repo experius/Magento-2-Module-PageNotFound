1c3d681e9e63ea753881d85c22cd6edb16a45d62
Bart Lubbersen <bart@experius.nl>
2017-09-15 14:14:24 +0000

    [TASK] Made from_url unique, if database already contains duplicates those are merged [TASK] If new redirect is added through back-end but from_url already exists the page with that url is updated and warning is shown [BUGFIX] There can't be duplicate from_url's because those caused the issue that only the first could be redirected

--- Api/PageNotFoundRepositoryInterface.php
+++ Api/PageNotFoundRepositoryInterface.php
@@ -22,6 +22,14 @@ interface PageNotFoundRepositoryInterface
 
     /**
      * Retrieve page_not_found
+     * @param string|int $pageNotFoundId
+     * @return \Experius\PageNotFound\Api\Data\PageNotFoundInterface
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function get($pageNotFoundId);
+
+    /**
+     * Retrieve page_not_found
      * @param string $pageNotFoundId
      * @return \Experius\PageNotFound\Api\Data\PageNotFoundInterface
      * @throws \Magento\Framework\Exception\LocalizedException
--- Model/ResourceModel/PageNotFound.php
+++ Model/ResourceModel/PageNotFound.php
@@ -15,4 +15,23 @@ class PageNotFound extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     {
         $this->_init('experius_page_not_found', 'page_not_found_id');
     }
+
+    /**
+     * Get pagenotfound identifier by from_url
+     *
+     * @param string $fromUrl
+     * @return int|false
+     */
+    public function getIdByFromUrl($fromUrl)
+    {
+        $connection = $this->getConnection();
+
+        $select = $connection->select()->from($this->getMainTable(), 'page_not_found_id')
+            ->where('from_url = :from_url');
+
+        $bind = [':from_url' => (string)$fromUrl];
+
+        return $connection->fetchOne($select, $bind);
+    }
+
 }
